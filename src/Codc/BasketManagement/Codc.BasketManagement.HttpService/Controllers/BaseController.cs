02bb5564a0aa3acdaf049fc70a6d2c70d099d9da
Kostadin Mandulov <k85@abv.bg>
2018-09-23 18:57:41 +0300

    initialize vs solution

---
+++ src/Codc/BasketManagement/Codc.BasketManagement.HttpService/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codc.BasketManagement.HttpService.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        protected string UserId
+        {
+            get
+            {
+                return this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            }
+        }
+    }
+}
---
+++ src/Codc/BasketManagement/Codc.BasketManagement.HttpService/Controllers/BasketsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Codc.BasketManagement.HttpService.Controllers
+{
+    [Route("api/[controller]")]
+    public class BasketsController : BaseController
+    {
+        [HttpGet]
+        public ActionResult<string> Test()
+        {
+            return "Hello, World!";
+        }
+    }
+}
